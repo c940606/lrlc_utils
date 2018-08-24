680ca559f7d3f4bf9511ba7cae47df46c34ef153
Mikhail Toptchi <mft@Mikhails-MBP.T-mobile.com>
2018-08-24 01:07:09 -0400

    initial commit

---
+++ lrlc_utils/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from lr_utils import load_dataset
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lrlc_utils',
+      version='0.1',
+      description='Mimic lr_utils',
+      url='http://github.com/haha867/lrlc_utils',
+      author='Flying Circus',
+      author_email='haha867@yandex.ru',
+      license='MIT',
+      packages=['lrlc_utils'],
+      zip_safe=False)# -*- coding: utf-8 -*-
+
